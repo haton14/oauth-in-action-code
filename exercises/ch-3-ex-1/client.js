e78f8da3840f10347c50e9662e1f9c914ec06781
haton14 <haton14@haton14.com>
2024-03-06 17:42:05 +0900

    OAuthクライアントの変更を追加

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,49 +27,66 @@ const authServer = {
  * Add the client information in here
  */
 const client = {
-	"client_id": "oauth-client-1",
-	"client_secret": "",
-	"redirect_uris": ["http://localhost:9000/callback"]
+	client_id: "oauth-client-1",
+	client_secret: "oauth-client-secret-1",
+	redirect_uris: ["http://localhost:9000/callback"]
 };
 
 const protectedResource = 'http://localhost:9002/resource';
 
-const state = null;
+const state = randomstring.generate();
 
-var access_token = null;
-var scope = null;
+let access_token = null;
+const scope = null;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.render('index', {access_token: access_token, scope: scope});
 });
 
-app.get('/authorize', function(req, res){
+app.get('/authorize', (req, res)=> {
 	const authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
 		response_type: 'code',
 		client_id: client.client_id,
-		redirect_uri: client.redirect_uris[0]
+		redirect_uri: client.redirect_uris[0],
+		state: state
 	});
 	res.redirect(authorizeUrl);
 });
 
-app.get('/callback', function(req, res){
-
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
-	
+app.get('/callback', (req, res)=> {
+	if (req.query.state !== state) {
+		res.render('error', {error: req.query.error});
+		return;
+	}
+	const code = req.query.code;
+	const form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});	
+	const headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		Authorization: `Basic ${encodeClientCredentials(client.client_id, client.client_secret)}`
+	};
+	const tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+	const body = JSON.parse(tokRes.getBody());
+	access_token = body.access_token;
+	res.render('index', {access_token: access_token, scope: scope});
 });
 
-app.get('/fetch_resource', function(req, res) {
+app.get('/fetch_resource', (req, res) => {
 });
 
-const buildUrl = function(base, options, hash) {
+const buildUrl = (base, options, hash) => {
 	const newUrl = url.parse(base, true);
-	delete newUrl.search;
+	newUrl.search = undefined;
 	if (!newUrl.query) {
 		newUrl.query = {};
 	}
-	__.each(options, function(value, key, list) {
+	__.each(options, (value, key, list) => {
 		newUrl.query[key] = value;
 	});
 	if (hash) {
@@ -79,13 +96,11 @@ const buildUrl = function(base, options, hash) {
 	return url.format(newUrl);
 };
 
-const encodeClientCredentials = function(clientId, clientSecret) {
-	return Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
-};
+const encodeClientCredentials = (clientId, clientSecret) => Buffer.from(`${querystring.escape(clientId)}:${querystring.escape(clientSecret)}`).toString('base64');
 
 app.use('/', express.static('files/client'));
 
-const server = app.listen(9000, 'localhost', function () {
+const server = app.listen(9000, 'localhost', () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('OAuth Client is listening at http://%s:%s', host, port);
