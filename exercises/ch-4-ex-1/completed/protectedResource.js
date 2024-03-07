76c423d82e7459cb74486227da731de374ffefc6
haton14 <haton14@haton14.com>
2024-03-07 23:29:57 +0900

    コードの変更を記述するための新しいファイルとパッケージの追加

--- exercises/ch-4-ex-1/completed/protectedResource.js
+++ exercises/ch-4-ex-1/completed/protectedResource.js
@@ -1,80 +1,75 @@
-var express = require("express");
-var url = require("url");
-var bodyParser = require('body-parser');
-var randomstring = require("randomstring");
-var cons = require('consolidate');
-var nosql = require('nosql').load('database.nosql');
-var qs = require("qs");
-var querystring = require('querystring');
-var request = require("sync-request");
-var __ = require('underscore');
-var base64url = require('base64url');
-var jose = require('jsrsasign');
-var cors = require('cors');
+const express = require("express");
+const url = require("url");
+const bodyParser = require("body-parser");
+const randomstring = require("randomstring");
+const cons = require("consolidate");
+const nosql = require("nosql").load("database.nosql");
+const qs = require("qs");
+const querystring = require("querystring");
+const request = require("sync-request");
+const __ = require("underscore");
+const base64url = require("base64url");
+const jose = require("jsrsasign");
+const cors = require("cors");
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
 
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
-app.set('views', 'files/protectedResource');
-app.set('json spaces', 4);
+app.engine("html", cons.underscore);
+app.set("view engine", "html");
+app.set("views", "files/protectedResource");
+app.set("json spaces", 4);
 
-app.use('/', express.static('files/protectedResource'));
+app.use("/", express.static("files/protectedResource"));
 app.use(cors());
 
-var resource = {
-	"name": "Protected Resource",
-	"description": "This data has been protected by OAuth 2.0"
+const resource = {
+	name: "Protected Resource",
+	description: "This data has been protected by OAuth 2.0",
 };
 
-var getAccessToken = function(req, res, next) {
-	
-	var inToken = null;
-	var auth = req.headers['authorization'];
-	if (auth && auth.toLowerCase().indexOf('bearer') == 0) {
-		inToken = auth.slice('bearer '.length);
-	} else if (req.body && req.body.access_token) {
+const getAccessToken = (req, res, next) => {
+	let inToken = null;
+	const auth = req.headers.authorization;
+	if (auth && auth.toLowerCase().indexOf("bearer") === 0) {
+		inToken = auth.slice("bearer ".length);
+	} else if (req.body?.access_token) {
 		// not in the header, check in the form body
 		inToken = req.body.access_token;
-	} else if (req.query && req.query.access_token) {
-		inToken = req.query.access_token
+	} else if (req.query?.access_token) {
+		inToken = req.query.access_token;
 	}
-	
-	console.log('Incoming token: %s', inToken);
-	nosql.one().make(function(builder) {
-	  builder.where('access_token', inToken);
-	  builder.callback(function(err, token) {
-	    if (token) {
-	      console.log("We found a matching token: %s", inToken);
-	    } else {
-	      console.log('No matching token was found.');
-	    };
-	    req.access_token = token;
-	    next();
-	    return;
-	  });
+
+	console.log("Incoming token: %s", inToken);
+	nosql.one().make((builder) => {
+		builder.where("access_token", inToken);
+		builder.callback((err, token) => {
+			if (token) {
+				console.log("We found a matching token: %s", inToken);
+			} else {
+				console.log("No matching token was found.");
+			}
+			req.access_token = token;
+			next();
+			return;
+		});
 	});
 };
 
-app.options('/resource', cors());
+app.options("/resource", cors());
 
-
-app.post("/resource", cors(), getAccessToken, function(req, res){
-
+app.post("/resource", cors(), getAccessToken, (req, res) => {
 	if (req.access_token) {
 		res.json(resource);
 	} else {
 		res.status(401).end();
 	}
-	
 });
 
-var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(9002, "localhost", () => {
+	const host = server.address().address;
+	const port = server.address().port;
 
-  console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
+	console.log("OAuth Resource Server is listening at http://%s:%s", host, port);
 });
- 
